6f5478fa7221c6f1d8638adc8b3a3ddee1937835
Nikesh Adhikari <nikeshad254@gmail.com>
2024-06-24 08:12:35 +0545

    chapter 7 : socket constructor and some getters

--- Networking/src/Chapter7.java
+++ Networking/src/Chapter7.java
@@ -1,11 +1,13 @@
 import java.io.*;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.*;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 // Chapter 7 - Server Socket
 public class Chapter7 {
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger("request");;
         try {
             ServerSocket serverSocket = new ServerSocket(13);
             Socket socket = serverSocket.accept();
@@ -31,14 +33,36 @@ public class Chapter7 {
             }
 
 
+            logger.warning("This is warning");
+            logger.fine("This is good");
 
             o.flush();
             socket.close();
+            serverSocket.close();
 
+            // how many can be pending (backlog)
+            ServerSocket serverSocket1 = new ServerSocket(80, 50);
+            serverSocket1.close();
 
+            // listens for connection on specified local interface.
+            InetAddress local = InetAddress.getByName("192.168.100.23");
+            ServerSocket serverSocket2 = new ServerSocket(80, 80, local);
+            serverSocket2.close();
 
+            // non argument constructor - won't understand which port to listen - thus need socket address & bind
+            ServerSocket serverSocket3 = new ServerSocket();
+            // thus we use bind to bind with the port
+            SocketAddress http = new InetSocketAddress(80);
+            serverSocket3.bind(http, 50);
+
+            System.out.println("Local socket Address: "+serverSocket3.getLocalSocketAddress());
+            System.out.println("Local port: "+serverSocket3.getLocalPort());
+            System.out.println("InetAddress: "+serverSocket3.getInetAddress());
+
+            serverSocket3.close();
 
         } catch (IOException e) {
+            logger.severe("Critical Error Occured");
             throw new RuntimeException(e);
         }
     }
