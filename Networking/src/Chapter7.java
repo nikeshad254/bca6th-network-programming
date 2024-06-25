5510b88aa708e721fe8011eee196ec8b39150ed2
Nikesh Adhikari <nikeshad254@gmail.com>
2024-06-25 07:57:38 +0545

    chapter 7 : socket options and performance ,chapter complete

--- Networking/src/Chapter7.java
+++ Networking/src/Chapter7.java
@@ -9,6 +9,7 @@ public class Chapter7 {
     public static void main(String[] args) {
         Logger logger = Logger.getLogger("request");;
         try {
+            /*
             ServerSocket serverSocket = new ServerSocket(13);
             Socket socket = serverSocket.accept();
 
@@ -49,6 +50,8 @@ public class Chapter7 {
             ServerSocket serverSocket2 = new ServerSocket(80, 80, local);
             serverSocket2.close();
 
+             */
+
             // non argument constructor - won't understand which port to listen - thus need socket address & bind
             ServerSocket serverSocket3 = new ServerSocket();
             // thus we use bind to bind with the port
@@ -58,6 +61,17 @@ public class Chapter7 {
             System.out.println("Local socket Address: "+serverSocket3.getLocalSocketAddress());
             System.out.println("Local port: "+serverSocket3.getLocalPort());
             System.out.println("InetAddress: "+serverSocket3.getInetAddress());
+
+            serverSocket3.setSoTimeout(100);
+            serverSocket3.setReuseAddress(true);
+            serverSocket3.setReceiveBufferSize(10);
+
+            System.out.println("Timeout: "+ serverSocket3.getSoTimeout());
+            System.out.println("Reuse Address: "+ serverSocket3.getReuseAddress());
+            System.out.println("Recieve BufferSize: "+ serverSocket3.getReceiveBufferSize());
+
+            // these are priority numbers which to give priority first here: bandwidth has high priority [1-less, 2-more, 3-high]
+            serverSocket3.setPerformancePreferences(2, 1, 3);
 
             serverSocket3.close();
 
