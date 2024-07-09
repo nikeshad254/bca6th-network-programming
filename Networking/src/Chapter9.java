8fff15916f8100cf5f8c482ca629485fcbbbc785
Nikesh Adhikari <nikeshad254@gmail.com>
2024-07-09 08:03:04 +0545

    chapter 9 : socket blocking and non blocking example of client and server

---
+++ Networking/src/Chapter9.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.channels.WritableByteChannel;
+
+public class Chapter9 {
+    public static void main(String[] args) {
+
+        // Client example of non blocking IO
+        SocketAddress socketAddress = new InetSocketAddress("www.google.com", 13);
+        try {
+            SocketChannel socketChannel = SocketChannel.open(socketAddress); // Blocking IO
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024); // Buffer size set
+             int socketRead = socketChannel.read(byteBuffer);
+            WritableByteChannel output = Channels.newChannel(System.out);
+            byteBuffer.flip();
+            output.write(byteBuffer);
+            byteBuffer.clear();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        // Server example of Non Blocking IO
+        ServerSocketChannel serverChannel = null;
+        try {
+            ServerSocket ss = serverChannel.socket();
+            ss.bind(new InetSocketAddress(19));
+
+            SocketChannel clientChannel = serverChannel.accept();
+            clientChannel.configureBlocking(false);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
