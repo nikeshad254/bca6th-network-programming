30e6e352b0173887ecc465d9e49e827565aeb2b3
Nikesh Adhikari <nikeshad254@gmail.com>
2024-08-05 08:05:25 +0545

    chapter 10 : datagramsocket constructor, socket options

--- Networking/src/Chapter10.java
+++ Networking/src/Chapter10.java
@@ -17,6 +17,32 @@ public class Chapter10 {
 
             System.out.println(new String(response.getData()));
 
+            // With port and interface address
+            DatagramSocket datagramSocket1 = new DatagramSocket(13, InetAddress.getByName("localhost"));
+            // With socket address
+            SocketAddress socketAddress = new InetSocketAddress(90);
+            DatagramSocket datagramSocket2 = new DatagramSocket(socketAddress);
+            datagramSocket2.close();
+
+            DatagramSocket datagramSocket3 = new DatagramSocket();
+            datagramSocket3.connect(InetAddress.getByName("localhost"), 80); // always have to send and recieve form this host and port
+            System.out.println(datagramSocket3.getPort());
+            System.out.println(datagramSocket3.getInetAddress());
+            System.out.println(datagramSocket3.getRemoteSocketAddress());
+
+            datagramSocket3.setSoTimeout(5000);
+            System.out.println(datagramSocket3.getSoTimeout());
+
+            datagramSocket3.setReceiveBufferSize(100);
+            datagramSocket3.setSendBufferSize(100);
+            datagramSocket3.setReuseAddress(true);
+            datagramSocket3.setBroadcast(true);
+            datagramSocket3.setTrafficClass(30); // 0 - 255
+
+
+            // datagramSocket3.send(new DatagramPacket());
+            // RECIEVE
+            datagramSocket3.close();
         } catch (SocketException e) {
             throw new RuntimeException(e);
         } catch (UnknownHostException e) {
